51171796255d05cacae9f1cdf657bbc8a35c6487
ChrisRusu1 <christopherrusu1@gmail.com>
2019-11-08 13:10:19 -0500

    Ask for inputs to the files

--- opencv track test/Source.cpp
+++ opencv track test/Source.cpp
@@ -6,41 +6,46 @@
 
 using namespace cv;
 using namespace std;
-
 int main(int argc, char *argv[])
 {
-	
 		Mat image;
 		Mat grayImage;
-
-		image = imread("kenrith.jpg");
+		Mat resizeImage;
+		string fileName;
+		cout << "input a file name(with extention)" << endl;
+		cin >> fileName;
+		image = imread(fileName);
 		if (!image.data) {
 			cout << "Could not open the image file" << endl;
 			return -1;
 		}
 		else {
+			cout << "please input a resizing factor(0-1)" << endl;
+			double resizeValue;
+			cin >> resizeValue;
 			int height = image.rows;
 			int width = image.cols;
 
 			cvtColor(image, grayImage, CV_BGR2GRAY);
-
-
-			namedWindow("Display window", WINDOW_NORMAL);
+			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue);
+			cout << "please input a text file name to store he ascii art, with .txt at the end" << endl;
+			string textfileName;
+			cin >> textfileName;
+			/*namedWindow("Display window", WINDOW_NORMAL);
 			imshow("Display window", image);
 
 			namedWindow("Gray Image", WINDOW_NORMAL);
-			imshow("Gray Image", grayImage);
+			imshow("Gray Image", resizeImage);*/
 			ofstream myfile;
-			myfile.open("example2.txt");
+			myfile.open(textfileName);
 			//myfile << "Writing this to a file.\n";
 			int avg;
 			int count = 0;
-			for (int i = 0; i < grayImage.rows; ++i) {// 0 is start
+			for (int i = 0; i < resizeImage.rows; ++i) {// 0 is start
 				i++;
-				const unsigned char* row = grayImage.ptr<unsigned char>(i);
-				for (int j = 0; j < grayImage.cols; j++) {//0 is start
+				for (int j = 0; j < resizeImage.cols; j++) {//0 is start
 					j++;
-					avg = grayImage.at<uchar>(i, j);
+					avg = resizeImage.at<uchar>(i, j);
 					if (avg < 29) myfile << '.';
 					else if (avg < 58) myfile << ',';
 					else if (avg < 87) myfile << ';';
