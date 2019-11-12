3e64cbdf130ba8da276a0ac9ae165b539a07a04d
ChrisRusu1 <christopherrusu1@gmail.com>
2019-11-11 19:33:39 -0500

    Insert your own ascii table, and standard ascii table

--- opencv track test/Source.cpp
+++ opencv track test/Source.cpp
@@ -3,6 +3,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <fstream>
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -14,7 +15,7 @@ int main(int argc, char *argv[])
 		string fileName;
 		cout << "input a file name(with extention)" << endl;
 		cin >> fileName;
-		image = imread(fileName);
+		image = imread("Images/"+fileName);
 		if (!image.data) {
 			cout << "Could not open the image file" << endl;
 			return -1;
@@ -27,48 +28,95 @@ int main(int argc, char *argv[])
 			int width = image.cols;
 
 			cvtColor(image, grayImage, CV_BGR2GRAY);
-			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue);
+			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue*.8, INTER_AREA);
 			cout << "please input a text file name to store he ascii art, with .txt at the end" << endl;
 			string textfileName;
 			cin >> textfileName;
+			
 			/*namedWindow("Display window", WINDOW_NORMAL);
 			imshow("Display window", image);
 
 			namedWindow("Gray Image", WINDOW_NORMAL);
 			imshow("Gray Image", resizeImage);*/
 			ofstream myfile;
-			myfile.open(textfileName);
+			myfile.open("textFile/"+textfileName);
 			//myfile << "Writing this to a file.\n";
-			int avg;
-			int count = 0;
-			for (int i = 0; i < resizeImage.rows; ++i) {// 0 is start
-				i++;
-				for (int j = 0; j < resizeImage.cols; j++) {//0 is start
-					j++;
-					avg = resizeImage.at<uchar>(i, j);
-					if (avg < 29) myfile << '.';
-					else if (avg < 58) myfile << ',';
-					else if (avg < 87) myfile << ';';
-					else if (avg < 116) myfile << '!';
-					else if (avg < 145) myfile << 'v';
-					else if (avg < 174) myfile << 'l';
-					else if (avg < 203) myfile << 'L';
-					else if (avg < 232) myfile << 'F';
-					else if (avg < 255) myfile << 'E';
-					else                      myfile << '$';
-						
+			cout << "do you want standard ascii values, or input your own? (o for own, s for standard)" << endl;
+			char choicec;
+			cin >> choicec;
+			string asciiSelf;
+			if (choicec == 'o') {
+				cout << "please input your ascii values (lighest to darkest)" << endl;
+				cin >> asciiSelf;
+				const int arraysize = asciiSelf.size();
+				vector<int> values;
+				double runs = 255 / arraysize;
+				for (int i = 0; i < arraysize; i++) {
+					values.push_back(runs * ((arraysize - (i + 1)) + 1));
 				}
-				myfile << endl;
+				values[0] = 255;
+				int avg;
+				for (int i = 0; i < resizeImage.rows; ++i) {// 0 is start
+					for (int j = 0; j < resizeImage.cols; j++) {//0 is start
+						avg = resizeImage.at<uchar>(i, j);
+						for (int i = 0; i < arraysize; i++) {
+							if (i == arraysize - 1) {
+								if (avg <= values[i] && avg >= 0) {
+									myfile << asciiSelf[i];
+									break;
+								}
+							}
+							else if (avg <= values[i] && avg >= values[i + 1]) {
+								myfile << asciiSelf[i];
+								break;
+							}
+						}
+					}
+					myfile << endl;
+				}
+				myfile.close();
+				image.release();
+				grayImage.release();
+				//cvWaitKey(0);
+				return 0;
 			}
-			
+			else {
+				char standard[10] = { ' ','.','-',':','=','+','*','#','%','@' };
+				int values[10];
+				double runs = 255 / 10;
+				for (int i = 0; i < 10; i++) {
+					values[10 - (i + 1)] = runs * (i + 1);
+				}
+				values[0] = 255;
+				int avg;
+				for (int i = 0; i < resizeImage.rows; i++) {// 0 is start
+					for (int j = 0; j < resizeImage.cols; j++) {//0 is start
+						avg = resizeImage.at<uchar>(i, j);
+						for (int l = 0; l < 10; l++) {
+							if (l == 9) {
+								if (avg <= values[l] && avg >= 0) {
+									myfile << standard[l];
+									break;
+								}
+							}
+							else if (avg <= values[l] && avg >= values[l + 1]) {
+								myfile << standard[l];
+								break;
+							}
+						}
+					}
+					myfile << endl;
+				}
+				myfile.close();
+				image.release();
+				grayImage.release();
+				//cvWaitKey(0);
+				return 0;
+			}
+				
 
+			// .'`^",:;Il!i><~+_-?][}{1)(\|?tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$ <<<white to black
 			
-			myfile.close();
-
-			//cvWaitKey(0);
-			image.release();
-			grayImage.release();
-			return 0;
 		}
 
 	}
