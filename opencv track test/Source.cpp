acd7980987b427eaebd73c93f7eb310cb3949ed5
ChrisRusu1 <christopherrusu1@gmail.com>
2019-11-12 14:46:05 -0500

    Make gifs

--- opencv track test/Source.cpp
+++ opencv track test/Source.cpp
@@ -3,121 +3,132 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <fstream>
+#include<sstream>  
 #include <vector>
+#include <windows.h>
+//#include <bits/stdc++.h>
 
 using namespace cv;
 using namespace std;
+
 int main(int argc, char *argv[])
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	for (int m = 0; m < 25; m++) {
+
+
 		Mat image;
 		Mat grayImage;
 		Mat resizeImage;
 		string fileName;
-		cout << "input a file name(with extention)" << endl;
-		cin >> fileName;
-		image = imread("Images/"+fileName);
+		int counter = m;
+		stringstream ss;
+
+		ss << counter;
+		ss >> fileName;
+		fileName = fileName + "_delay-0.09s.jpg";
+		if (counter >= 10) {
+			fileName = "Images/gen/frame_" + fileName;
+		}
+		else {
+			fileName = "Images/gen/frame_0" + fileName;
+		}
+
+
+		//cout << "input a file name(with extention)" << endl;
+		//cin >> fileName;
+		image = imread(fileName);
 		if (!image.data) {
-			cout << "Could not open the image file" << endl;
+			cout << "Could not open the image file " << fileName << endl;
+			cvWaitKey(0);
 			return -1;
 		}
 		else {
-			cout << "please input a resizing factor(0-1)" << endl;
-			double resizeValue;
-			cin >> resizeValue;
+			double resizeValue = 0.046;
 			int height = image.rows;
 			int width = image.cols;
 
 			cvtColor(image, grayImage, CV_BGR2GRAY);
-			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue*.7, INTER_AREA);
-			cout << "please input a text file name to store he ascii art, with .txt at the end" << endl;
+			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue, INTER_AREA);
 			string textfileName;
-			cin >> textfileName;
-			
-			/*namedWindow("Display window", WINDOW_NORMAL);
-			imshow("Display window", image);
+			textfileName = "genie";
+			textfileName += to_string(m);
 
-			namedWindow("Gray Image", WINDOW_NORMAL);
-			imshow("Gray Image", resizeImage);*/
+			cout << textfileName << endl;
 			ofstream myfile;
-			myfile.open("textFile/"+textfileName);
+			myfile.open("textFile/genie/" + textfileName);
 			//myfile << "Writing this to a file.\n";
-			cout << "do you want standard ascii values, or input your own? (o for own, s for standard)" << endl;
 			char choicec;
-			cin >> choicec;
 			string asciiSelf;
-			if (choicec == 'o') {
-				cout << "please input your ascii values (lighest to darkest)" << endl;
-				cin >> asciiSelf;
-				const int arraysize = asciiSelf.size();
-				vector<int> values;
-				double runs = 255 / arraysize;
-				for (int i = 0; i < arraysize; i++) {
-					values.push_back(runs * ((arraysize - (i + 1)) + 1));
-				}
-				values[0] = 255;
-				int avg;
-				for (int i = 0; i < resizeImage.rows; ++i) {// 0 is start
-					for (int j = 0; j < resizeImage.cols; j++) {//0 is start
-						avg = resizeImage.at<uchar>(i, j);
-						for (int i = 0; i < arraysize; i++) {
-							if (i == arraysize - 1) {
-								if (avg <= values[i] && avg >= 0) {
-									myfile << asciiSelf[i];
-									break;
-								}
-							}
-							else if (avg <= values[i] && avg >= values[i + 1]) {
-								myfile << asciiSelf[i];
-								break;
-							}
-						}
-					}
-					myfile << endl;
-				}
-				myfile.close();
-				image.release();
-				grayImage.release();
-				//cvWaitKey(0);
-				return 0;
+
+			char standard[10] = { ' ','.','-',':','=','+','*','#','%','@' };
+			int values[10];
+			double runs = 255 / 10;
+			for (int i = 0; i < 10; i++) {
+				values[10 - (i + 1)] = runs * (i + 1);
 			}
-			else {
-				char standard[10] = { ' ','.','-',':','=','+','*','#','%','@' };
-				int values[10];
-				double runs = 255 / 10;
-				for (int i = 0; i < 10; i++) {
-					values[10 - (i + 1)] = runs * (i + 1);
-				}
-				values[0] = 255;
-				int avg;
-				for (int i = 0; i < resizeImage.rows; i++) {// 0 is start
-					for (int j = 0; j < resizeImage.cols; j++) {//0 is start
-						avg = resizeImage.at<uchar>(i, j);
-						for (int l = 0; l < 10; l++) {
-							if (l == 9) {
-								if (avg <= values[l] && avg >= 0) {
-									myfile << standard[l];
-									break;
-								}
-							}
-							else if (avg <= values[l] && avg >= values[l + 1]) {
+			values[0] = 255;
+			int avg;
+			for (int i = 0; i < resizeImage.rows; i++) {// 0 is start
+				for (int j = 0; j < resizeImage.cols; j++) {//0 is start
+					avg = resizeImage.at<uchar>(i, j);
+					for (int l = 0; l < 10; l++) {
+						if (l == 9) {
+							if (avg <= values[l] && avg >= 0) {
 								myfile << standard[l];
 								break;
 							}
 						}
+						else if (avg <= values[l] && avg >= values[l + 1]) {
+							myfile << standard[l];
+							break;
+						}
 					}
-					myfile << endl;
 				}
-				myfile.close();
-				image.release();
-				grayImage.release();
-				//cvWaitKey(0);
-				return 0;
+				myfile << endl;
 			}
-				
+			myfile.close();
+			image.release();
+			grayImage.release();
+			//cvWaitKey(0);
 
-			// .'`^",:;Il!i><~+_-?][}{1)(\|?tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$ <<<white to black
-			
 		}
+	}
+	while (true) {
+		for (int n = 0; n < 25; n++) {
+			
+			cout << string(50, '\n');
+			string line;///make a string for the line
+			string textfileName = "genie";
+			textfileName += to_string(n);
+
+			ifstream InFile(("textFile/genie/" + textfileName).c_str()); /// just a text file
+			while (getline(InFile, line)) /// getline returns false at EOF
+			{
+
+				string inputStr(line);
+				string buf; /// Have a buffer string
+				stringstream s(inputStr); /// Insert the string into a stream
+				s << inputStr;///put the line into the stream
+				string input;///string for the input
+
+				while (getline(s, input))///gets a string from the stream up the next space
+				{
+					
+						cout <<input;
+					
+
+				}
+				cout << '\n';
+			}
+			Sleep(90);
+		}
+		
 
 	}
+	return 0;
+
+
+}
 
