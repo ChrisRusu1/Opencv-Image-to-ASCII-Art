3da82140a9d8d7605b1c98cc5ae4e06a8c9f3a65
ChrisRusu1 <christopherrusu1@gmail.com>
2020-02-02 14:41:30 -0500

    more images

--- opencv track test/Source.cpp
+++ opencv track test/Source.cpp
@@ -7,8 +7,7 @@
 
 using namespace cv;
 using namespace std;
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 		Mat image;
 		Mat grayImage;
 		Mat resizeImage;
@@ -28,7 +27,7 @@ int main(int argc, char *argv[])
 			int width = image.cols;
 
 			cvtColor(image, grayImage, CV_BGR2GRAY);
-			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue*.7, INTER_AREA);
+			resize(grayImage, resizeImage, cv::Size(), resizeValue, resizeValue*.6, INTER_AREA);
 			cout << "please input a text file name to store he ascii art, with .txt at the end" << endl;
 			string textfileName;
 			cin >> textfileName;
